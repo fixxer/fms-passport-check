b6610da98eb68c7157e97f489cc99c835bb9a679
fixxer <fixxer@inbox.ru>
2015-01-15 23:20:22 +0300

    Two tests share fixture

--- indexing-tool/src/test/clojure/tests.clj
+++ indexing-tool/src/test/clojure/tests.clj
@@ -1,13 +1,11 @@
 (ns tests
     (:use clojure.test fixxer.fms.index))
 
-(deftest test-byte-seq
-  (let [istream (java.io.ByteArrayInputStream. (byte-array 5 [1 2 3 4 5]))]
-    (is (= (take 5 (byte-seq istream)) '(1 2 3 4 5)))))
-
-(deftest test-byte-seq-eof
-  (let [istream (java.io.ByteArrayInputStream. (byte-array 5 [1 2 3 4 5]))]
-    (is (= (take 6 (byte-seq istream)) '(1 2 3 4 5)))))
+(let [istreamf #(java.io.ByteArrayInputStream. (byte-array 5 [1 2 3 4 5]))]
+  (deftest test-byte-seq
+    (is (= (take 5 (byte-seq (istreamf))) '(1 2 3 4 5))))
+  (deftest test-byte-seq-eof
+    (is (= (take 6 (byte-seq (istreamf))) '(1 2 3 4 5)))))
 
 (deftest test-merged-seq1
   (is (= (merged-seq [1 3] [2 4] compare) '(1 2 3 4))))
